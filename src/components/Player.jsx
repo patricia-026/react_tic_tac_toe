334bc1441a12b67f2dc5af982df9b70b713ab103
Patricia <kovach.patri@gmail.com>
2023-11-08 10:34:09 +0100

    best practice:updating state based on old state correctly

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,13 +4,13 @@ export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        setIsEditing(!isEditing);
+        setIsEditing((editing) => !editing);
     }
 
     let playerName = <span className='player-name'>{name}</span>;
 
     if (isEditing) {
-        playerName = <input type="text" required value={name} />;
+        playerName = <input type="text" required defaultValue={name} />;
     }
 
     return (
