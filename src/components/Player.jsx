cc3ac6723be3ea365507470daa773e9b06446ad0
Patricia <kovach.patri@gmail.com>
2023-11-08 10:09:30 +0100

    conditional content

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,17 +4,13 @@ export default function Player({ name, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
-        if (!isEditing) {
-            setIsEditing(true);
-            return;
-        }
-        setIsEditing(false);
+        setIsEditing(!isEditing);
     }
 
     let playerName = <span className='player-name'>{name}</span>;
 
     if (isEditing) {
-        playerName = <input type="text" required />;
+        playerName = <input type="text" required value={name} />;
     }
 
     return (
@@ -23,7 +19,7 @@ export default function Player({ name, symbol }) {
                 {playerName}
                 <span className='player-symbol'>{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     );
 }
