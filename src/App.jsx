7619e95db356d655929ea7a6e3cd232b5ae29197
Patricia <kovach.patri@gmail.com>
2023-11-10 17:02:40 +0100

    an alternative to lifting state up

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,8 @@ function deriveActivePlayer(gameTurns) {
 
 function App() {
   const [players, setPlayers] = useState({
-    'X': 'Player 1',
-    'O': 'Player 2'
+    X: 'Player 1',
+    O: 'Player 2'
   });
   const [gameTurns, setGameTurns] = useState([]);
   //const [activePlayer, setActivePlayer] = useState('X');
@@ -52,7 +52,7 @@ function App() {
       firstSquareSymbol === secondSquareSymbol &&
       firstSquareSymbol === thirdSquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   }
 
@@ -89,8 +89,8 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange} />
+          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange} />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
